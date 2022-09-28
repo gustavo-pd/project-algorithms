cc8e195a0a9b130cddf1cd885c3a3e1819b8c51e
GustavoPD <gudalmoro@gmail.com>
2022-09-28 19:46:08 -0300

    find duplicate

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,13 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
+    f_string_list = list(first_string.lower())
+    s_string_list = list(second_string.lower())
+
+    for string in f_string_list:
+        if string in s_string_list:
+            s_string_list.remove(string)
+        else:
+            return False
+    if len(s_string_list) != 0:
+        return False
+    else:
+        return True
--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,14 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
+    if len(nums) < 2 or not nums or not isinstance(nums, list):
+        return False
+
+    dict_nums = {}
+    for n in nums:
+        if n in dict_nums:
+            return n
+        if isinstance(n, str) or n < 0 or n == "":
+            return False
+
+        dict_nums[n] = 1
+
+    return False
