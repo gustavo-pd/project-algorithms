3a0ef294c328c4ade8a29da811322a2035f0437f
GustavoPD <gudalmoro@gmail.com>
2022-09-28 18:59:03 -0300

    palindrome

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,13 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
+
+    if word[high_index] != word[low_index]:
+        return False
+
+    if not word:
+        return False
+
+    if high_index <= low_index:
+        return True
+
+    else:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
